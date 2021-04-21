b29e083e1e82f7194c5a56b248c93fae36941bd9
Saad <72356508+Izazul-Haque@users.noreply.github.com>
2021-04-21 11:54:26 +0600

    Add files via upload

---
+++ bloodlink/aboutus.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<style>
+    <?php include 'css/au_css.css' ?>
+</style>
+
+<?php $title="BloodLink | About Us"; ?>
+<?php require 'head.php'; ?>
+<body>
+<?php require 'header.php'; ?>
+
+<div class="about-section">
+    <h1 class="ml1">
+    <span class="text-wrapper">
+        <span class="line line1"></span>
+        <span class="letters">About Us</span>
+        <span class="line line2"></span>
+    </span>
+    </h1>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="image/saad.jpg"  style="width:100%">
+      <div class="container">
+        <h2>Izazul Haque Saad</h2>
+        <p class="title">181-15-10621</p>
+        <p>izazul15-10621@diu.edu.bd</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="image/himel.jpg" style="width:100%">
+      <div class="container">
+        <h2>Isa Khan Himel</h2>
+        <p class="title">181-15-10948</p>
+        <p>himel15-10948@diu.edu.bd</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img src="image/farhan.jpg"  style="width:100%">
+      <div class="container">
+        <h2>Md. Mazharul Islam</h2>
+        <p class="title">181-15-10648</p>
+        <p>mazharul15-10648@diu.edu.bd</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="image/naim.jpg" style="width:100%">
+      <div class="container">
+        <h2>Md. Nafiul Islam</h2>
+        <p class="title">181-15-10694</p>
+        <p>nafiul15-10694@diu.edu.bd</p>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+    <?php include 'js/script.js' ?>
+</script>
+<?php require 'footer.php'; ?>
+</body>
+</html>
---
+++ bloodlink/footer.php
@@ -0,0 +1,10 @@
+<style>
+    <?php include 'css/style.css'; ?>
+</style>
+<footer class="footer text-center">
+	© Copyright <span id="demo"></span> <span class="brand">BloodLink. </span> All Rights Reserved.
+	<script>
+		date = new Date();
+    	document.getElementById("demo").innerHTML=date.getFullYear();
+    </script>
+</footer>
